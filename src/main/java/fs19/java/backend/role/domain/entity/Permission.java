efb7e66b2c56aa004758e1ed51665745bb193849
NandaLochana <danushkanandalochana@gmail.com>
2024-11-12 13:51:46 +0200

    role and role permission entities committed

---
+++ src/main/java/fs19/java/backend/role/domain/entity/Permission.java
@@ -0,0 +1,22 @@
+package fs19.java.backend.role.domain.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.UUID;
+
+/**
+ * Responsible to work as a base model for a permission object
+ */
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+@Setter
+public class Permission {
+
+    private UUID id;
+    private String name;
+
+}
