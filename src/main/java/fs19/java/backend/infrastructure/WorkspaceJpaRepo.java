24e239cee0d986bb748a6711633f661fdb0f7805
nagadheerajb <nagadheerajb@gmail.com>
2024-11-21 19:05:10 +0200

    workspace feature to jpa from inmemory

---
+++ src/main/java/fs19/java/backend/infrastructure/WorkspaceJpaRepo.java
@@ -0,0 +1,9 @@
+package fs19.java.backend.infrastructure;
+
+import fs19.java.backend.domain.entity.Workspace;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.UUID;
+
+public interface WorkspaceJpaRepo extends JpaRepository<Workspace, UUID> {
+}
